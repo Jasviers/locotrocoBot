13f965539313432bc67f1befa7f098c510512707
jasviers <jasviers@gmail.com>
2021-11-16 21:40:00 +0100

    Change all commands to operate with musicPlayer

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="-")
+bot = commands.Bot(command_prefix="¡")
 
 @bot.event
 async def on_ready():
--- src/musicPlayer.py
+++ src/musicPlayer.py
@@ -1,11 +1,32 @@
 import discord
-import youtube_dl
+from discord.ext import commands
+from youtube_dl import YoutubeDL
 
 import asyncio
 from async_timeout import timeout
 import random
-import itertools
 from functools import partial
+from urllib.parse import urlparse
+
+
+ytdlopts = {
+    'format': 'bestaudio',
+    'noplaylist': 'True'}
+
+ffmpegopts = {
+    'before_options': '-nostdin',
+    'options': '-vn'
+}
+
+ytdl = YoutubeDL(ytdlopts)
+
+
+class VoiceConnectionError(commands.CommandError):
+    """Custom Exception class for connection errors."""
+
+
+class InvalidVoiceChannel(VoiceConnectionError):
+    """Exception for cases of invalid Voice Channels."""
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, requester):
@@ -16,46 +37,44 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.web_url = data.get('webpage_url')
         self.duration = data.get('duration')
 
-        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-        self.ytdl = youtube_dl.YoutubeDL(self.YDL_OPTIONS)
-
     def __getitem__(self, item: str):
         return self.__getattribute__(item)
 
     @classmethod
-    async def create_source(cls, ctx, search: str, *, loop, download=False):
+    async def create_source(cls, ctx, search: str, *, loop):
         loop = loop or asyncio.get_event_loop()
 
-        to_run = partial(cls.ytdl.extract_info, url=search, download=download)
-        data = await loop.run_in_executor(None, to_run)
+        url_result = urlparse(search)
+        if all([url_result.scheme, url_result.netloc, url_result.path]):
+            to_run = partial(ytdl.extract_info, url=search, download=False)
+            data = await loop.run_in_executor(None, to_run)
+        else:
+            to_run = partial(ytdl.extract_info, url=f"ytsearch:{search}", download=False)
+            data = await loop.run_in_executor(None, to_run)
 
         if 'entries' in data:
             data = data['entries'][0]
 
         embed = discord.Embed(title="", description=f"Queued [{data['title']}]({data['webpage_url']}) [{ctx.author.mention}]", color=discord.Color.green())
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed, delete_after=60)
 
-        if download:
-            source = cls.ytdl.prepare_filename(data)
-        else:
-            return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
-
-        return cls(discord.FFmpegPCMAudio(source), data=data, requester=ctx.author)
+       
+        return {'webpage_url': data['webpage_url'], 'requester': ctx.author, 'title': data['title']}
 
     @classmethod
     async def regather_stream(cls, data, *, loop):
         loop = loop or asyncio.get_event_loop()
         requester = data['requester']
 
-        to_run = partial(cls.ytdl.extract_info, url=data['webpage_url'], download=False)
+        to_run = partial(ytdl.extract_info, url=data['webpage_url'], download=False)
         data = await loop.run_in_executor(None, to_run)
 
         return cls(discord.FFmpegPCMAudio(data['url']), data=data, requester=requester)
 
 
 class MusicPlayer:
-    def __init__(self, bot, ctx):
-        self.bot = bot
+    def __init__(self, ctx):
+        self.bot = ctx.bot
         self.ctx = ctx
         self.channel = self.ctx.channel
         self.guild = self.ctx.guild
@@ -68,7 +87,7 @@ class MusicPlayer:
         self.np = None
         self.volume = 0.5
 
-        self.ctx.loop.create_task(self.player_loop())
+        self.bot.loop.create_task(self.player_loop())
 
     async def player_loop(self):
         await self.bot.wait_until_ready()
@@ -80,7 +99,7 @@ class MusicPlayer:
                 async with timeout(300):
                     source = await self.queue.get()
             except asyncio.TimeoutError:
-                return self.destroy(self._guild)
+                return self.destroy(self.guild)
 
             if not isinstance(source, YTDLSource):
                 try:
@@ -95,10 +114,11 @@ class MusicPlayer:
 
             self.guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
             embed = discord.Embed(title="Now playing", description=f"[{source.title}]({source.web_url}) [{source.requester.mention}]", color=discord.Color.green())
-            self.np = await self._channel.send(embed=embed)
+            self.np = await self.channel.send(embed=embed)
             await self.next.wait()
 
             source.cleanup()
+            await self.np.delete()
             self.actual_song = None
 
     def shuffle(self):
